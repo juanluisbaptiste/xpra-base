97a74062e3544f7259a63da6ab2b69dfc17d5456
Christopher Law <chris@chrislaw.me>
2020-12-22 22:12:07 +0000

    Updated python access point to sqlite_auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,8 @@ if [ "${ENABLE_WEB_VIEW}" == "yes" ]; then
     ${XPRA}
   else
     #Credentials have been provided so create password file and link to it
-    python /usr/lib/python2.7/dist-packages/xpra/server/auth/sqlite_auth.py ./auth.sdb create
-    python /usr/lib/python2.7/dist-packages/xpra/server/auth/sqlite_auth.py ./auth.sdb add ${XPRA_USER} ${XPRA_PASSWORD}
+    python /usr/lib/python3/dist-packages/xpra/server/auth/sqlite_auth.py ./auth.sdb create
+    python /usr/lib/python3/dist-packages/xpra/server/auth/sqlite_auth.py ./auth.sdb add ${XPRA_USER} ${XPRA_PASSWORD}
     XPRA="${XPRA} --auth=sqlite:filename=./auth.sdb --ws-auth=sqlite:filename=./auth.sdb --tcp-auth=sqlite:filename=./auth.sdb"
     ${XPRA}
   fi
